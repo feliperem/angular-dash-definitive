4633ca5c88e2a479ce9d62ad9981cd8a4ad51a20
Felipe Rodrigues <felipe.eufrasio@outloook.com>
2021-03-03 03:16:08 -0300

    Adição dos tutoriais e graficos na aba detalhes

--- src/app/main/details/details.component.ts
+++ src/app/main/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  label1: string = 'Cozinha/Lavanderia';
+  label2: string = 'Quarto/Sala';
+
+  apar1: string = 'Liquidificador';
+  apar2: string = 'Air Fryer';
+  apar3: string = 'Geladeira';
+  apar4: string = 'Aspirador de pó';
+
+  Highcharts = Highcharts;
+
+  charts1: {};
+  charts2: {};
+
+  constructor() {
+    this.charts1 = {};
+    this.charts2 = {};
+   }
 
   ngOnInit(): void {
+    this.charts2 = {
+      chart: {
+          type: 'area'
+      },
+      title: {
+        text: null
+      },
+      subtitle: {
+        text: null
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      xAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      yAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      series: [{
+          data: [70, 10, 56, 50, ]
+      }]
+    };
   }
 
 }
